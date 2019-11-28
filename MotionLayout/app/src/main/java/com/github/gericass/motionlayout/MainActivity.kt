88798c9f9d36bc3fe673d9e1bc4cde329348f718
gericass <bonjovi1926@yahoo.co.jp>
2019-11-28 20:20:12 +0900

    add: motion layout

--- MotionLayout/app/src/main/java/com/github/gericass/motionlayout/MainActivity.kt
+++ MotionLayout/app/src/main/java/com/github/gericass/motionlayout/MainActivity.kt
@@ -2,11 +2,14 @@ package com.github.gericass.motionlayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.container
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        container.loadLayoutDescription(R.xml.test_motion)
+
     }
 }
